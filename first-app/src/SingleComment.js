5abe6d1d1785eb3590ff2138544e3786b537bb6e
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-04-13 22:11:40 +0530

    second

--- first-app/src/SingleComment.js
+++ first-app/src/SingleComment.js
@@ -1,20 +1,20 @@
 import React from "react";
 import profile1 from "./image/profile1.jpg";
 
-const SingleComment = () => {
+const SingleComment = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={profile1} alt="profile picture" />
+        <img src={props.picture} alt="profile picture" />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Shailesh
+          {props.name}
         </a>
         <div className="metadata">
-          <span className="date">Today at 12:00PM</span>
+          <span className="date">{props.date}</span>
         </div>
-        <div className="text">fantabulous and nice</div>
+        <div className="text">{props.text}</div>
       </div>
     </div>
   );
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import profile1 from "./image/profile1.jpg";
-import SingleComment from './SingleComment';
+import profile2 from "./image/profile2.jpg"
+import SingleComment from "./SingleComment";
 
 const App = () => {
   const style = { backgroundColor: "black", color: "white" };
 
   return (
     <div className="ui comments">
-      <SingleComment />
-      <SingleComment />
-      <SingleComment />
+      <SingleComment name="Aditya" date="Today at 4PM" text="It is amazing" picture={profile1} />
+      <SingleComment name="Yogesh" date="Today at 2AM" text="Awesome" picture={profile2} />
+      <SingleComment name="Bob" date="Today at 12AM" text="Thanks....." picture={profile1} />
     </div>
   );
 };
