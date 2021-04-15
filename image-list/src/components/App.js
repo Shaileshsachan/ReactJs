6b304904d0a74833c3ba37e50c904b871ee963ef
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-04-15 21:41:56 +0530

    done

---
+++ image-list/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SearchInput from "./SearchInput";
+
+class App extends React.Component {
+    onSearchSubmit = (entry) => {
+        console.log(entry)
+    }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "30px" }}>
+        <SearchInput onSearchSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
