32f72036892d80193b091d0c5c3e7553fe92e16b
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-04-15 14:04:47 +0530

    react lifecycle

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Hemisphere.css";
+import NorthernHem from "./images/NorthernHem.jpg";
+import SoutthernHem from "./images/SouthernHem.jpg";
 
-const HemisphereDisplay = () => {
-  return <div>Hey you are in Northern Hemisphere!</div>;
+const HemisphereConfig = {
+  Northern: {
+    text: "It is northern Hemisphere",
+    picture: NorthernHem,
+  },
+  Southern: {
+    text: "It i Southern Hemisphere",
+    picture: SoutthernHem,
+  },
+};
+
+const HemisphereDisplay = ({ latitude }) => {
+  const hemisphere = latitude > 0 ? "Northern" : "Southern";
+  const { text, picture } = HemisphereConfig[hemisphere];
+
+  return (
+    <div className={hemisphere}>
+      <div className="ui text container segment">
+        <div className='image'>
+          <img src={picture} alt="" />
+        </div>
+        <div className='ui teal bottom attached label'>
+          <h1>{text}</h1>
+          </div>
+      </div>
+    </div>
+  );
 };
 
 export default HemisphereDisplay;
--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
+import HemisphereDisplay from "./HemisphereDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    latitude: null,
+    errorMessage: "",
+  };
 
-    this.state = {latitude:null, errorMessage: ''}
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ latitude: position.coords.latitude });
@@ -21,7 +24,7 @@ class App extends React.Component {
       return <div>{this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.latitude) {
-      return <div>{this.state.latitude}</div>;
+      return <div><HemisphereDisplay latitude={this.state.latitude} /></div>;
     } else {
       return <div>Loading...</div>;
     }
