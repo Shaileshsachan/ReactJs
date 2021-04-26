e485c9c596f01e0f5a5378647cc98972068c8b49
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-04-26 13:47:56 +0530

    done

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import axios from "axios";
 import SearchInput from "./SearchInput";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
 
-  async onSearchSubmit(entry) {
+  onSearchSubmit = async (entry) => {
     const response = await axios.get(
       `https://pixabay.com/api/?key=21299977-c2cfb69a28bfc508ed32e8c4c&q=${entry}&image_type=photo`
     );
     this.setState({ images: response.data.hits });
-  }
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "30px" }}>
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
-        We have {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
