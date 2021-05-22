08a81c666170280147c344471a5d50e9b7066588
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-05-22 14:57:13 +0530

    mapstate to props

--- react-router/src/components/Card.js
+++ react-router/src/components/Card.js
@@ -1,24 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Card extends React.Component {
-  state = { user: "" };
+  // state = { user: "" };
 
-  componentDidMount() {
-    let user = this.props.match.params.user;
-    this.setState({ user });
-  }
+  // componentDidMount() {
+  //   let user = this.props.match.params.user;
+  //   this.setState({ user });
+  // }
 
   render() {
-    const { user } = this.state;
+    // const { user } = this.state;
+    const { title, body } = this.props.card;
     return (
       <div
         className="ui raised very padded text container segment"
         style={{ marginTop: "80px" }}
       >
-        <h3 className="ui header">{user}</h3>
+        <h3 className="ui header">{title}</h3>
+        <p>{body}</p>
       </div>
     );
   }
 }
 
-export default Card;
+const mapStateToProps = (state, ownProps) => {
+  let title = ownProps.match.params.user;
+  return {
+    card: state.cards.find((card) => card.title === title),
+  };
+};
+export default connect(mapStateToProps)(Card);
